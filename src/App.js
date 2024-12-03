a12b3ff3936c699c35c517dc58fb83e4f09535a6
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2024-12-03 01:45:46 -0300

    Add Componentes App.js e NavBar.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import Body from './components/Body';
+import './styles/reset.css';
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
